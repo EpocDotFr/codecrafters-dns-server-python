21320e9ca9f5b9ed842597bf171fe59635ea14c5
EpocDotFr <contact@epoc.fr>
2023-11-30 17:10:10 +0100

    Stage 1?

--- app/message.py
+++ app/message.py
@@ -1,5 +1,4 @@
-from typing import BinaryIO, Tuple, Any
-from app.utils import byte_to_bits
+from typing import BinaryIO, Tuple
 from dataclasses import dataclass
 import struct
 
@@ -38,9 +37,22 @@ class Header(HasStructMixin):
     _format = 'H2sHHHH'
 
     def serialize(self, f: BinaryIO) -> None:
+        bits = ''
+
+        bits += '1' if self.query_response else '0'
+        bits += format(self.operation_code, '04b')
+        bits += '1' if self.authoritative_answer else '0'
+        bits += '1' if self.truncated_message else '0'
+        bits += '1' if self.recursion_desired else '0'
+        bits += '1' if self.recursion_available else '0'
+        bits += format(self.reserved, '03b')
+        bits += format(self.response_code, '04b')
+
+        bits = int(bits, 2).to_bytes(2, byteorder='big')
+
         f.write(self.pack(
             self.packet_id,
-            b'00',
+            bits,
             self.question_count,
             self.answer_count,
             self.authority_count,
@@ -51,20 +63,18 @@ class Header(HasStructMixin):
     def unserialize(cls, f: BinaryIO):
         packet_id, bits, question_count, answer_count, authority_count, additional_count = cls.unpack(f.read(cls.size()))
 
-        bits = ''.join([byte_to_bits(b) for b in bits])
-
-        print(bits)
+        bits = ''.join([format(b, '08b') for b in bits])
 
         return cls(
             packet_id=packet_id,
             query_response=bits[0] == '1',
-            operation_code=int(bits[1:4], 2),
+            operation_code=int(bits[1::4], 2),
             authoritative_answer=bits[6] == '1',
             truncated_message=bits[7] == '1',
             recursion_desired=bits[8] == '1',
             recursion_available=bits[9] == '1',
-            reserved=int(bits[10:3], 2),
-            response_code=int(bits[14:4], 2),
+            reserved=int(bits[10::3], 2),
+            response_code=int(bits[14::4], 2),
             question_count=question_count,
             answer_count=answer_count,
             authority_count=authority_count,
--- app/utils.py
+++
@@ -1,6 +0,0 @@
-def byte_to_bits(b: bytes) -> str:
-    return format(int.from_bytes(b, byteorder='big'), '08b')
-
-
-def bits_to_byte(b: str) -> bytes:
-    return int(b, 2).to_bytes(4, byteorder='big')
